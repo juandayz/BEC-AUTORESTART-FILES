2a7af95387f30ce422f15df002ac755c7c9633c2
juan <17104227+juandayz@users.noreply.github.com>
2018-02-10 17:07:01 -0300

    Update NW_SHUTDOWN.bat

--- BattlEye/Bec/AUTORESTART_FILES/NW_SHUTDOWN.bat
+++ BattlEye/Bec/AUTORESTART_FILES/NW_SHUTDOWN.bat
@@ -2,9 +2,7 @@
 echo SHUTDOWN ARMA2OA SERVER
 TIMEOUT /T 2 /NOBREAK
 taskkill /F /IM arma2oaserver.exe
-TIMEOUT /T 4 /NOBREAK
-echo SHUTDOWN DEAD BEC
-taskkill /f /im cmd.exe /fi "windowtitle eq NW_BEC_CONTROL
+
 
 set cleanuplogs="C:\DZE_Server_Config\BattlEye\Bec\AUTORESTART_FILES"
 cd /d %cleanuplogs%
