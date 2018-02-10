90878536acc82a4ce18cb3bc467e882c4c144a3d
juan <17104227+juandayz@users.noreply.github.com>
2018-02-10 17:06:30 -0300

    Update NW_BEC_CONTROL.bat

--- BattlEye/Bec/AUTORESTART_FILES/NW_BEC_CONTROL.bat
+++ BattlEye/Bec/AUTORESTART_FILES/NW_BEC_CONTROL.bat
@@ -1,6 +1,12 @@
+:top
+TIMEOUT /T 1 /NOBREAK
 
-
-TIMEOUT /T 110 /NOBREAK
-cd\
-cd "C:\DZE_Server_Config\BattlEye\Bec"
+cd C:\DZE_Server_Config\BattlEye\Bec
 Bec.exe -f Config.cfg --dsc
+
+ping 127.0.0.1 -n 2 >NUL
+start /wait "" /min "C:\DZE_Server_Config\BattlEye\Bec" -f config.cfg --dsc
+
+ping 127.0.0.1 -n 2 >NUL
+echo DO NOT WORRY BEC IS TRYING TO RECONECT...
+goto top 
