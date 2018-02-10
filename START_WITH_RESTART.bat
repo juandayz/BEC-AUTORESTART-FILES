25f71e26040c2e6afca32686b6c12898047ed618
juan <17104227+juandayz@users.noreply.github.com>
2018-02-10 17:05:30 -0300

    Update START_WITH_RESTART.bat

--- START_WITH_RESTART.bat
+++ START_WITH_RESTART.bat
@@ -8,6 +8,11 @@ set DB_PASSWORD="YourMysqlPass"
 set DB_NAME="YourDataBaseName"
 
 ::END SETTINGS
+
+cd %becPath%
+echo (%time%) LOADING BEC.
+start "NW_BEC_CONTROL" "NW_BEC_CONTROL.bat"
+
 :arma2oaserver
 echo NW MONITOR STARTING...
 FOR /F "tokens=1-4 DELIMS=/ " %%F IN ('date /T') DO (set v_date=%%F%%G%%H)
@@ -17,9 +22,7 @@ set fname=database_%v_date%_%v_time%.sql
 ping 127.0.0.1 -n 5 >NUL
 
 title Monitor 
-cd %becPath%
-echo (%time%) LOADING BEC.
-start "NW_BEC_CONTROL" "NW_BEC_CONTROL.bat"
+
 cd %serverPath%
 echo (%time%) NW SERVER STARTING.
 start /wait "arma2" /min "C:\epoch\arma2oaserver.exe" -port=2302 -autoInit -noSound -noPause "-config=C:\DZE_Server_Config\24_napf.cfg" "-cfg=C:\DZE_Server_Config\basic.cfg" "-profiles=C:\DZE_Server_Config" -name=DZE_Server_Config "-mod=@DayZ_Epoch;@DayZ_Epoch_Server;" 
